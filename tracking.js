dfe718e1b165c4c9b5ef6226b60c8c5411dc73fc
yo <contact@yoanndefay.com>
2022-03-18 18:53:14 +0700

    j

--- tracking.js
+++ tracking.js
@@ -3,7 +3,7 @@ console.log("changing discord invite link")
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
 
-const discordID = params.invite;
+const discordID = params["utm_campaign"] || params["utm_source"] || params["invite"];
 
 if (discordID) {
     const links = document.querySelectorAll('a[href*="https://discord.gg"]');
